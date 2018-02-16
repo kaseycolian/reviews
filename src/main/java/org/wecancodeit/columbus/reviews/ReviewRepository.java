6f32a13a25582160ef54d773647f0050e4e35ba6
Kasey <KaseyColian@gmail.com>
2018-02-16 11:08:41 -0500

    Repository map started

--- src/main/java/org/wecancodeit/columbus/reviews/ReviewRepository.java
+++ src/main/java/org/wecancodeit/columbus/reviews/ReviewRepository.java
@@ -1,5 +1,29 @@
 package org.wecancodeit.columbus.reviews;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ReviewRepository {
+
+	public Map<Long, Review> reviews = new HashMap<>();
+
+	public ReviewRepository() {
+		Review godspeedYou = new Review(1997L, "Godspeed You Black Emporer", "http://cstrecords.com/gybe/", "Music",
+				"Experimental Band", "Amazing");
+
+		reviews.put(godspeedYou.getProductId(), godspeedYou);
+	}
+
+	public ReviewRepository(Review firstProduct) {
+
+	}
+
+ public Review findOne(long firstProductId) {
+
+return reviews.get(productId);
+ }
 
 }
--- src/test/java/org/wecancodeit/columbus/reviews/ReviewRepositoryTest.java
+++ src/test/java/org/wecancodeit/columbus/reviews/ReviewRepositoryTest.java
@@ -1,6 +1,23 @@
 package org.wecancodeit.columbus.reviews;
 
-public class ReviewRepositoryTest {
-	private ReviewRepository underTest = new ReviewRepository();
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
+public class ReviewRepositoryTest {
+	private ReviewRepository underTest;
+	
+	private long firstProductId = 123;
+	private Review firstProduct = new Review (firstProductId, "g2", "www.g2.com", "black pen", "inkers", "gelly");
+	
+	@Test
+	public void shouldFindFirstReview() {
+		underTest = new ReviewRepository (firstProduct);
+		Review result = underTest.findOne(firstProductId);
+		assertThat (result, is(firstProduct));
+	}
+
+	
+	
 }
