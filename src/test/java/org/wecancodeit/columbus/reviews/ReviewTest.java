b1608aa003262fe8261f8585d04b9ffa5709a2c9
Kasey <KaseyColian@gmail.com>
2018-02-16 10:30:52 -0500

    Review Class done and Tests passed.  May change refactor variables to mean other things

---
+++ src/test/java/org/wecancodeit/columbus/reviews/ReviewTest.java
@@ -0,0 +1,48 @@
+package org.wecancodeit.columbus.reviews;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ReviewTest {
+
+	Review underTest = new Review(1234L, "pen", "www.url.com", "pointy", "ink", "gelpen");
+
+	@Test
+	public void shouldReturnId() {
+		Long productId = underTest.getProductId();
+		assertThat(productId, is(1234L));
+	}
+
+	@Test
+	public void shouldReturnTitle() {
+		String title = underTest.getTitle();
+		assertThat(title, is("pen"));
+	}
+
+	@Test
+	public void shouldReturnImageUrl() {
+		String imageUrl = underTest.getImageUrl();
+		assertThat(imageUrl, is("www.url.com"));
+	}
+
+	@Test
+	public void shouldReturnCategory() {
+		String category = underTest.getCategory();
+		assertThat(category, is("pointy"));
+
+	}
+
+	@Test
+	public void shouldReturnContent() {
+		String content = underTest.getContent();
+		assertThat(content, is("ink"));
+	}
+
+	@Test
+	public void shoulReturnDescription() {
+		String description = underTest.getDescription();
+		assertThat(description, is("gelpen"));
+	}
+}
