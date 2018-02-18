651e005b332aee9acaf04ac1e593a536f8359a8f
Kasey <KaseyColian@gmail.com>
2018-02-18 18:03:02 -0500

    Image & video sized & located properly.

--- src/test/java/org/wecancodeit/columbus/reviews/ReviewControllerTest.java
+++ src/test/java/org/wecancodeit/columbus/reviews/ReviewControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
 
 public class ReviewControllerTest {
 
@@ -12,11 +13,25 @@ public class ReviewControllerTest {
 	private ReviewController underTest;
 
 	@Mock
+	private ReviewRepository repository;
+
+	@Mock
+	private Model model;
+
+	@Mock
 	private Review review;
+
+	@Mock
+	private Review anotherReview;
 
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void shouldGetASingleReviewModel() {
+
 	}
 
 	@Test
