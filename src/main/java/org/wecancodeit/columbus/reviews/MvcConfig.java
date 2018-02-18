99ba29fb49ca391ebd17a655917372647c1fcc3f
Kasey <KaseyColian@gmail.com>
2018-02-18 02:27:17 -0500

    images working

--- src/main/java/org/wecancodeit/columbus/reviews/MvcConfig.java
+++ src/main/java/org/wecancodeit/columbus/reviews/MvcConfig.java
@@ -1,20 +1,20 @@
-package org.wecancodeit.columbus.reviews;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Configuration
-@EnableWebMvc
-public class MvcConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-          .addResourceHandler("/resources/**")
-          .addResourceLocations("/resources/static/images/")
-          .addResourceLocations("/resources/"); 
-    }
-}
+//package org.wecancodeit.columbus.reviews;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebMvc
+//public class MvcConfig extends WebMvcConfigurerAdapter {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//          .addResourceHandler("/resources/**")
+//          .addResourceLocations("/resources/static/images/")
+//          .addResourceLocations("/resources/"); 
+//    }
+//}
 
 
